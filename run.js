31a6ce9ce1e8f13ed6d1ed04834b729a6083f886
anonmousrdp2 <66356917+anonmousrdp2@users.noreply.github.com>
2024-08-17 15:04:21 +0530

    Update run.js

--- run.js
+++ run.js
@@ -1,12 +1,9 @@
-// Function to fetch data from the PHP file
 function fetchData() {
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://raw.githubusercontent.com/anonmousrdp2/anonmousrdp2/main/who.php", true);
+    xhr.open("GET", "https://anonmousrdp2.github.io/who.php", true);
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4 && xhr.status == 200) {
-            // Output the result in the console or display it on the page
             console.log(xhr.responseText);
-            // Example of appending the response to the body
             var p = document.createElement("p");
             p.textContent = xhr.responseText;
             document.body.appendChild(p);
@@ -15,5 +12,4 @@ function fetchData() {
     xhr.send();
 }
 
-// Call the function to execute the PHP file
 fetchData();
